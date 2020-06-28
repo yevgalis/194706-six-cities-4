34a729d5cc1ce2aa9726a77efa8861f3d7eec8ae
Yevgeniy Alishevets <yevgalis@protonmail.com>
2020-06-28 22:33:13 +0600

    added property component and react-router-dom

--- src/components/main/main.test.js
+++ src/components/main/main.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import Main from './main.jsx';
 
@@ -8,20 +9,25 @@ it(`Main page correctly renders`, () => {
     id: 1,
     title: `Appartment listing`,
     type: `Apartment`,
+    bedrooms: 1,
+    capacity: 2,
     price: 110,
     rating: `100%`,
     isPremium: true,
+    features: [`Wi-Fi`, `Towels`, `Heating`, `Cabel TV`, `Fridge`],
     imgSrc: `img/apartment-01.jpg`,
     coordinates: [52.3709553943508, 4.89309666406198]
   }];
 
   const tree = renderer
     .create(
-        <Main
-          offersCount={offersCount}
-          offers={offers}
-          onCardTitleClick={() => {}}
-        />, {
+        <BrowserRouter>
+          <Main
+            offersCount={offersCount}
+            offers={offers}
+            onCardTitleClick={() => {}}
+          />
+        </BrowserRouter>, {
           createNodeMock: () => document.createElement(`div`)
         }
     )
--- src/components/places-list/places-list.test.js
+++ src/components/places-list/places-list.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import PlacesList from './places-list.jsx';
 
@@ -7,19 +8,24 @@ it(`PlacesList component renders correctly`, () => {
     id: 1,
     title: `Appartment listing`,
     type: `Apartment`,
+    bedrooms: 1,
+    capacity: 2,
     price: 110,
     rating: `100%`,
     isPremium: true,
+    features: [`Wi-Fi`, `Towels`, `Heating`, `Cabel TV`, `Fridge`],
     imgSrc: `img/apartment-01.jpg`,
     coordinates: [52.3709553943508, 4.89309666406198]
   }];
 
   const tree = renderer
     .create(
-        <PlacesList
-          offers={offers}
-          onCardTitleClick={() => {}}
-        />
+        <BrowserRouter>
+          <PlacesList
+            offers={offers}
+            onCardTitleClick={() => {}}
+          />
+        </BrowserRouter>
     )
     .toJSON();
 
